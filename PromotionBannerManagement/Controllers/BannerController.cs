76ed564c04fd98e6dc323aa0bb0e9300fae0bc42
sarbashreeeeee <sapkota.sarbashree222@gmail.com>
2025-04-08 01:13:33 +0545

    corrected error in the BannerController class

--- PromotionBannerManagement/Controllers/BannerController.cs
+++ PromotionBannerManagement/Controllers/BannerController.cs
@@ -1,65 +1,66 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using PromotionBannerManagement.DTO;
-//using PromotionBannerManagement.Models;
-//using PromotionBannerManagement.Service;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using PromotionBannerManagement.DTO;
+using PromotionBannerManagement.Models;
+using PromotionBannerManagement.Service;
 
-//namespace PromotionBannerManagement.Controllers
-//{
-//    [ApiController]
-//    [Route("[controller]")]
-//    public class BannerController
-//    {
-//        private IBannerService _bannerService;
-//        public BannerController(IBannerService bannerService)
-//        {
-//            _bannerService = bannerService;
-//        }
+namespace PromotionBannerManagement.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BannerController : ControllerBase
+    {
+        private IBannerService _bannerService;
+        public BannerController(IBannerService bannerService)
+        {
+            _bannerService = bannerService;
+        }
 
-//        [HttpPost("add-banner")]
-//        public IActionResult AddBanner(BannerDTO bannerInfo)
-//        {
-//            var isBannerCreated = _bannerService.AddBanner(bannerInfo);
+        [HttpPost("add-banner")]
+        public IActionResult AddBanner(BannerDTO bannerInfo)
+        {
+            var isBannerCreated = _bannerService.AddBanner(bannerInfo);
 
-//            if (isBannerCreated)
-//            {
-//                return Ok("Banner created");
-//            }
-//            return BadRequest("Unable to create banner");
-//        }
+            if (isBannerCreated)
+            {
+                return Ok("Banner created");
+            }
+            return BadRequest("Unable to create banner");
+        }
 
-//        [HttpGet("get-banners")]
-//        public List<Banner> GetBanners()
-//        {
-//            var bannerList = _bannerService.GetBanners();
-//            return bannerList;
-//        }
+        [HttpGet("get-banners")]
+        public List<Banner> GetBanners()
+        {
+            var bannerList = _bannerService.GetBanners();
+            return bannerList;
+        }
 
-//        [HttpPut("update-banner-title")]
-//        public IActionResult UpdateBannerTitle(int bannerId, string newTitle)
-//        {
-//            var isBannerTitleUpdated = _bannerService.UpdateBannerTitle(bannerId, newTitle);
-//            if (isBannerTitleUpdated)
-//            {
-//                return Ok("Banner title updated successfully");
-//            }
-//            else
-//            {
-//                return BadRequest("Unable to update banner title");
-//            }
-//        }
+        [HttpPut("update-banner-title")]
+        public IActionResult UpdateBannerTitle(int bannerId, string newTitle)
+        {
+            var isBannerTitleUpdated = _bannerService.UpdateBannerTitle(bannerId, newTitle);
+            if (isBannerTitleUpdated)
+            {
+                return Ok("Banner title updated successfully");
+            }
+            else
+            {
+                return BadRequest("Unable to update banner title");
+            }
+        }
 
-//        [HttpDelete("delete-banner")]
-//        public IActionResult DeleteBanner(int bannerId)
-//        {
-//            var isBannerDeleted = _bannerService.DeleteBanner(bannerId);
-//            if (isBannerDeleted)
-//            {
-//                return Ok($"Banner {bannerId} deleted successfully");
-//            }
-//            else
-//            {
-//                return BadRequest("Unable to delete banner");
-//            }
-//        }
-//    }
-//}
+        [HttpDelete("delete-banner")]
+        public IActionResult DeleteBanner(int bannerId)
+        {
+            var isBannerDeleted = _bannerService.DeleteBanner(bannerId);
+            if (isBannerDeleted)
+            {
+                return Ok($"Banner {bannerId} deleted successfully");
+            }
+            else
+            {
+                return BadRequest("Unable to delete banner");
+            }
+        }
+    }
+}
